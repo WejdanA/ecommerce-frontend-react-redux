dd9f7c70f36f49fe40eb361a303f453c63cc3b1a
WejdanA <lol.lol.1417@hotmail.com>
2023-12-26 08:37:47 +0300

    delete product

--- src/components/admin/products/ProductsManager.tsx
+++ src/components/admin/products/ProductsManager.tsx
@@ -1,17 +1,26 @@
-import { Link } from 'react-router-dom'
+import { useEffect } from 'react'
 import { Accordion } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
+import { baseURL } from '../../../api'
+import {
+  clearProductMessage,
+  deleteProduct,
+  fetchProductsData
+} from '../../../redux/slices/productSlice'
 import { AppDispatch, RootState } from '../../../redux/store'
-import { removeItem } from '../../../redux/slices/cartSlice'
-import { removeProduct } from '../../../redux/slices/productSlice'
-
+import { Messages } from '../../../utils/Messages'
 import { Admin } from '../Admin'
 import { NewProductWrapper } from './NewProductWrapper'
 
 export function ProductsManager() {
+  const { products, isLoading, error, success } = useSelector((state: RootState) => state.products)
   const dispatch = useDispatch<AppDispatch>()
-  const products = useSelector((state: RootState) => state.products)
+
+  useEffect(() => {
+    dispatch(fetchProductsData())
+  }, [dispatch])
 
   return (
     <div className="main-content">
@@ -28,52 +37,68 @@ export function ProductsManager() {
           </Accordion>
         </div>
 
-        {products.isLoading && <h3> Loading products...</h3>}
-        <div className="local-bootstrap">
-          {' '}
-          <table className="table table-striped">
-            <thead className="thead-dark">
-              <tr>
-                <th scope="col">id</th>
-                <th scope="col">name</th>
-                <th scope="col">price</th>
-                <th scope="col">image</th>
-                <th scope="col">Variants</th>
-                <th scope="col">sizes</th>
-                <th scope="col">operations</th>
-              </tr>
-            </thead>
+        {isLoading ? (
+          <h3> Loading products...</h3>
+        ) : (
+          <div className="local-bootstrap">
+            {products?.length ? (
+              <table className="table table-striped">
+                <thead className="thead-dark">
+                  <tr>
+                    <th scope="col">id</th>
+                    <th scope="col">name</th>
+                    <th scope="col">price</th>
+                    <th scope="col">image</th>
+                    <th scope="col">quantity</th>
+                    <th scope="col">sold</th>
+                    <th scope="col">categories</th>
+                    <th scope="col">operations</th>
+                  </tr>
+                </thead>
 
-            <tbody>
-              {products.fetchedProducts.map((product) => (
-                <tr key={product.id}>
-                  <td>{product.id}</td>
-                  <td>{product.name}</td>
-                  <td>{product.price}</td>
-                  <td>
-                    <img src={product.image} alt={product.name} width="50" />
-                  </td>
-                  <td>{product.variants.join(',')}</td>
-                  <td>{product.sizes.join(',')}</td>
-                  <td>
-                    <Link to={`/admin/products/${product.id}`}>
-                      <button className="edit-btn">Edit</button>
-                    </Link>
-                    <button
-                      className="remove-btn"
-                      onClick={() => {
-                        dispatch(removeProduct(product.id))
-                        dispatch(removeItem(product.id))
-                      }}>
-                      X
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+                <tbody>
+                  {products.map((product) => (
+                    <tr key={product._id}>
+                      <td>{product._id}</td>
+                      <td>{product.name}</td>
+                      <td>{product.price}</td>
+                      <td>
+                        <img src={baseURL + product.image} alt={product.name} width="50" />
+                      </td>
+                      <td>{product.quantity}</td>
+                      <td>{product.sold}</td>
+                      <td>
+                        {product.categories &&
+                          product.categories.map((category) => (
+                            <span key={category._id} id="category">
+                              {category.name + ' '}
+                            </span>
+                          ))}
+                      </td>
+
+                      <td>
+                        <Link to={`/admin/products/${product._id}`}>
+                          <button className="edit-btn">Edit</button>
+                        </Link>
+                        <button
+                          className="remove-btn"
+                          onClick={() => {
+                            dispatch(deleteProduct(product._id))
+                          }}>
+                          X
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            ) : (
+              <h3> No Products Were Found</h3>
+            )}
+          </div>
+        )}
       </div>
+      <Messages error={error} success={success} clearMessage={clearProductMessage} />
     </div>
   )
 }
