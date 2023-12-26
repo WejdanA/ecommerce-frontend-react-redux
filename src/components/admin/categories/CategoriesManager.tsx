c98b6668f8e78cc4e7a0ff7aa990178e78c8524d
WejdanA <lol.lol.1417@hotmail.com>
2023-12-26 07:07:27 +0300

    categories CRUD operations

--- src/components/admin/categories/CategoriesManager.tsx
+++ src/components/admin/categories/CategoriesManager.tsx
@@ -1,23 +1,35 @@
 import { Accordion } from 'react-bootstrap'
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useForm, SubmitHandler } from 'react-hook-form'
 
 import { AppDispatch, RootState } from '../../../redux/store'
-import { addCategory, editCategory, removeCategory } from '../../../redux/slices/categorySlice'
-
+import {
+  clearCategoryMessage,
+  createCategory,
+  deleteCategory,
+  fetchCategories,
+  updateCategory
+} from '../../../redux/slices/categorySlice'
 import { Admin } from '../Admin'
+import { Messages } from '../../../utils/Messages'
 
 type Inputs = {
   category: string
 }
 export function CategoriesManager() {
+  const { categories, isLoading, success, error } = useSelector(
+    (state: RootState) => state.categories
+  )
   const dispatch = useDispatch<AppDispatch>()
-  const categories = useSelector((state: RootState) => state.categories)
 
   const [categoryName, setCategoryName] = useState('')
-  const [categoryId, setCategoryId] = useState(0)
+  const [categoryId, setCategoryId] = useState('')
   const [btnText, setBtnText] = useState('Create')
+
+  useEffect(() => {
+    dispatch(fetchCategories())
+  }, [dispatch])
 
   const {
     register,
@@ -30,18 +42,17 @@ export function CategoriesManager() {
   }
 
   const addCategoryHandle: SubmitHandler<Inputs> = (data) => {
-    if (btnText == 'create') {
-      const category = { id: +new Date(), name: data.category }
-      dispatch(addCategory(category))
+    if (btnText == 'Create') {
+      dispatch(createCategory(data.category))
     } else {
-      const category = { id: categoryId, name: categoryName }
-      dispatch(editCategory(category))
+      const category = { _id: categoryId, name: categoryName }
+      dispatch(updateCategory(category))
       setBtnText('Create')
       setCategoryName('')
     }
   }
 
-  const editCategoryHandle = (categoryId: number, categoryName: string) => {
+  const editCategoryHandle = (categoryId: string, categoryName: string) => {
     setCategoryId(categoryId)
     setCategoryName(categoryName)
     setBtnText('Update')
@@ -51,78 +62,82 @@ export function CategoriesManager() {
     <div className="main-content">
       <Admin />
       <div id="products" className="products-container">
-        {categories.isLoading && <h3> Loading categories...</h3>}
-        <div className="card grid gap-4">
-          <div className="local-bootstrap">
-            <Accordion className="admin-form">
-              <Accordion.Item eventKey="0">
-                <Accordion.Header>addCategory</Accordion.Header>
-                <Accordion.Body>
-                  <div className="contact">
-                    <form
-                      action="submit"
-                      onSubmit={handleSubmit(addCategoryHandle)}
-                      className="contact-form category">
-                      <div className="input-group">
-                        <input
-                          id="category"
-                          {...register('category', { required: true })}
-                          name="category"
-                          value={categoryName}
-                          onChange={(e) => changeCategoryHandle(e)}
-                          placeholder="category name"
-                        />
-                        {errors.category && <span className="error"></span>}
-                      </div>
+        {isLoading ? (
+          <h3> Loading categories...</h3>
+        ) : (
+          <div className="card grid gap-4">
+            <div className="local-bootstrap">
+              <Accordion className="admin-form">
+                <Accordion.Item eventKey="0">
+                  <Accordion.Header>addCategory</Accordion.Header>
+                  <Accordion.Body>
+                    <div className="contact">
+                      <form
+                        action="submit"
+                        onSubmit={handleSubmit(addCategoryHandle)}
+                        className="contact-form category">
+                        <div className="input-group">
+                          <input
+                            id="category"
+                            {...register('category', { required: true })}
+                            name="category"
+                            value={categoryName}
+                            onChange={(e) => changeCategoryHandle(e)}
+                            placeholder="category name"
+                          />
+                          {errors.category && <span className="error"></span>}
+                        </div>
 
-                      <input type="submit" value={btnText + ' category'} className="form-btn" />
-                    </form>
-                  </div>
-                </Accordion.Body>
-              </Accordion.Item>
-            </Accordion>
-          </div>
-          <div className="local-bootstrap">
-            <table className=" table table-stripe">
-              <thead className="thead-dark">
-                <tr>
-                  <th scope="col">id</th>
-                  <th scope="col">name</th>
-                  <th scope="col">edit</th>
-                  <th scope="col">delete</th>
-                </tr>
-              </thead>
-              <tbody>
-                {' '}
-                {categories.categories.map((category) => (
-                  <tr key={category.id}>
-                    <td>category.id</td>
-                    <td>{category.name}</td>
-
-                    <td>
-                      <button
-                        className=" edit-btn"
-                        onClick={() => editCategoryHandle(category.id, category.name)}>
-                        Edit
-                      </button>
-                    </td>
-
-                    <td>
-                      <button
-                        className=" remove-btn"
-                        onClick={() => {
-                          dispatch(removeCategory(category.id))
-                        }}>
-                        X
-                      </button>
-                    </td>
+                        <input type="submit" value={btnText + ' category'} className="form-btn" />
+                      </form>
+                    </div>
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
+            </div>
+            <div className="local-bootstrap">
+              <table className=" table table-stripe">
+                <thead className="thead-dark">
+                  <tr>
+                    <th scope="col">id</th>
+                    <th scope="col">name</th>
+                    <th scope="col">edit</th>
+                    <th scope="col">delete</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {' '}
+                  {categories.map((category) => (
+                    <tr key={category._id}>
+                      <td>{category._id}</td>
+                      <td>{category.name}</td>
+
+                      <td>
+                        <button
+                          className=" edit-btn"
+                          onClick={() => editCategoryHandle(category._id, category.name)}>
+                          Edit
+                        </button>
+                      </td>
+
+                      <td>
+                        <button
+                          className=" remove-btn"
+                          onClick={() => {
+                            dispatch(deleteCategory(category._id))
+                          }}>
+                          X
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
-        </div>
+        )}
       </div>
+      <Messages error={error} success={success} clearMessage={clearCategoryMessage} />
     </div>
   )
 }
--- src/redux/slices/categorySlice.ts
+++ src/redux/slices/categorySlice.ts
@@ -1,57 +1,162 @@
-import { createSlice } from '@reduxjs/toolkit'
-import { ProductType } from './productSlice'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 
-export type CategoryType = {
-  id: number
-  name: string
-}
-
-export type CategoryState = {
-  categories: CategoryType[]
-  error: null | string
-  isLoading: boolean
-  category: CategoryType | {} | undefined
-}
+import api from '../../api'
+import { CategoryState, CategoryType } from '../../types/categoryTypes'
 
 const initialState: CategoryState = {
   categories: [],
   error: null,
+  success: null,
   isLoading: false,
   category: {}
 }
+
+// user fetch categories fetch category
+export const fetchCategories = createAsyncThunk(
+  './category/fetchCategories',
+  async (_, { rejectWithValue }) => {
+    try {
+      const { data } = await api.get('/categories')
+
+      return data
+    } catch (error: any) {
+      if (!error.status && error.message === 'Network Error') {
+        return rejectWithValue('Network Error')
+      }
+      return rejectWithValue(error.response.data.msg)
+    }
+  }
+)
+
+export const fetchCategory = createAsyncThunk(
+  './category/fetchCategory',
+  async (_id: string, { rejectWithValue }) => {
+    try {
+      const { data } = await api.get(`/categories/${_id}`)
+
+      return data
+    } catch (error: any) {
+      if (!error.status && error.message === 'Network Error') {
+        return rejectWithValue('Network Error')
+      }
+      return rejectWithValue(error.response.data.msg)
+    }
+  }
+)
+
+// admin - create category, update category, delete category
+export const createCategory = createAsyncThunk(
+  './category/createCategory',
+  async (name: string, { rejectWithValue }) => {
+    try {
+      const { data } = await api.post(`/categories/`, { name })
+
+      return data
+    } catch (error: any) {
+      if (!error.status && error.message === 'Network Error') {
+        return rejectWithValue('Network Error')
+      }
+      return rejectWithValue(error.response.data.msg || error.response.data.errors[0])
+    }
+  }
+)
+
+export const updateCategory = createAsyncThunk(
+  './category/updateCategory',
+  async (updatedCategory: CategoryType, { rejectWithValue }) => {
+    const { _id, name } = updatedCategory
+    try {
+      const { data } = await api.put(`/categories/${_id}`, { name })
+
+      return data
+    } catch (error: any) {
+      if (!error.status && error.message === 'Network Error') {
+        return rejectWithValue('Network Error')
+      }
+      return rejectWithValue(error.response.data.msg || error.response.data.errors[0])
+    }
+  }
+)
+
+export const deleteCategory = createAsyncThunk(
+  './category/deleteCategory',
+  async (_id: string, { rejectWithValue }) => {
+    try {
+      const { data } = await api.delete(`/categories/${_id}`)
+
+      return data
+    } catch (error: any) {
+      if (!error.status && error.message === 'Network Error') {
+        return rejectWithValue('Network Error')
+      }
+
+      return rejectWithValue(error.response.data.msg)
+    }
+  }
+)
 
 export const categorySlice = createSlice({
   name: 'category',
   initialState,
   reducers: {
-    categoriesRequest: (state) => {
-      state.isLoading = true
-    },
-    categoriesSuccess: (state, action) => {
-      state.isLoading = false
-      state.categories = action.payload
-    },
-    addCategory: (state, action) => {
-      state.categories = [action.payload, ...state.categories]
-    },
-
-    editCategory: (state, action) => {
-      const editedCategory = action.payload
-      const updatedCategories = state.categories.map((category) => {
-        if (category.id == editedCategory.id) {
-          return editedCategory
-        }
-        return category
-      })
-      state.categories = updatedCategories
-    },
-
-    removeCategory: (state, action) => {
-      state.categories = state.categories.filter((category) => category.id !== action.payload)
+    clearCategoryMessage: (state) => {
+      state.success = null
+      state.error = null
     }
+  },
+  extraReducers: (builder) => {
+    builder
+      // user - fetch categories, fetch category
+      .addCase(fetchCategories.fulfilled, (state, action) => {
+        state.categories = action.payload.allCategories
+        state.isLoading = false
+      })
+      .addCase(fetchCategory.fulfilled, (state, action) => {
+        state.categories = action.payload.category
+        state.isLoading = false
+      })
+
+      // admin - create category, update category, delete category
+      .addCase(createCategory.fulfilled, (state, action) => {
+        const { message, category } = action.payload
+        state.categories = [category, ...state.categories]
+        state.isLoading = false
+        state.success = message
+      })
+      .addCase(updateCategory.fulfilled, (state, action) => {
+        const { message, updatedCategory } = action.payload
+
+        const updatedCategories = state.categories.map((category) => {
+          if (category._id == updatedCategory._id) {
+            return updatedCategory
+          }
+          return category
+        })
+        state.categories = updatedCategories
+        state.isLoading = false
+        state.success = message
+      })
+      .addCase(deleteCategory.fulfilled, (state, action) => {
+        const { message, _id } = action.payload
+        state.categories = state.categories.filter((category) => category._id !== _id)
+        state.isLoading = false
+        state.success = message
+      })
+      .addMatcher(
+        (action) => action.type.endsWith('/pending'),
+        (state) => {
+          state.isLoading = true
+        }
+      )
+      .addMatcher(
+        (action) => action.type.endsWith('/rejected'),
+        (state, action) => {
+          state.isLoading = false
+          state.error = action.payload
+        }
+      )
   }
 })
-export const { categoriesRequest, categoriesSuccess, addCategory, editCategory, removeCategory } =
-  categorySlice.actions
+export const { clearCategoryMessage } = categorySlice.actions
 
 export default categorySlice.reducer
